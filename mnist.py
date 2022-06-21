905c1c5fbea84e7b257678294f94ad18bed487b2
eljam3239 <elijames2301@gmail.com>
2022-06-21 14:01:56 -0400

    Simple CNN to learn setting up the layers

---
+++ mnist.py
@@ -0,0 +1,45 @@
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Dense, Input
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten
+from keras import backend as k
+import matplotlib.pyplot as plt
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+fig = plt.figure()
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    plt.tight_layout()
+    plt.imshow(X_train[i], cmap='gray', interpolation='none')
+    plt.title("Digit: {}".format(y_train[i]))
+    plt.xticks([])
+    plt.yticks([])
+
+#reshaping
+#this assumes our data format
+#For 3D data, "channels_last" assumes (conv_dim1, conv_dim2, conv_dim3, channels) while 
+#"channels_first" assumes (channels, conv_dim1, conv_dim2, conv_dim3).
+img_rows, img_cols = 28,28
+
+if k.image_data_format() == 'channels_first':
+    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
+    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+#more reshaping
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+print('X_train shape:', X_train.shape) #X_train shape: (60000, 28, 28, 1)
+
+y_train = keras.utils.to_categorical(y_train)
+y_test = keras.utils.to_categorical(y_test)
+
+model = Sequential()
