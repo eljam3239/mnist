9489036802baf6470ea0dcbe528aa38bd9eacced
eljam3239 <elijames2301@gmail.com>
2022-06-21 14:46:08 -0400

    Working MNISt cnn

--- mnist.py
+++ mnist.py
@@ -2,6 +2,7 @@ import numpy as np
 import keras
 from keras.datasets import mnist
 from keras.models import Model
+from keras.models import Sequential
 from keras.layers import Dense, Input
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten
 from keras import backend as k
@@ -39,7 +40,31 @@ X_train /= 255
 X_test /= 255
 print('X_train shape:', X_train.shape) #X_train shape: (60000, 28, 28, 1)
 
-y_train = keras.utils.to_categorical(y_train)
-y_test = keras.utils.to_categorical(y_test)
+num_category = 10
+y_train = keras.utils.to_categorical(y_train, num_category)
+y_test = keras.utils.to_categorical(y_test, num_category)
 
 model = Sequential()
+model.add(Conv2D(32, kernel_size=(3,3),activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, (3,3), activation='relu'))
+#choose the best features via pooling
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(Dropout(0.25))
+#flatten to get 1 output
+model.add(Flatten())
+#connect to get all relevant data
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+#output a softmax to squash the matrix into output probabilities
+model.add(Dense(num_category, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+
+batch_size = 128
+num_epoch = 10
+model.log = model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epoch, verbose=1, validation_data=(X_test, y_test))
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
